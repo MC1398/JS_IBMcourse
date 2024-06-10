efa0c5f2505fcfa52a60a14c54771c97ba88b432
MC1398 <matiascerruti13@gmail.com>
2024-06-10 16:15:27 -0400

    2nd lab practica asignaciones

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -12,9 +12,9 @@ const blockConst = "I'm a block-scoped const";
 }
 
 // Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+//console.log(globalVar); // Output: "I'm a global variable"
+//console.log(globalLet); // Output: "I'm also global, but scoped with let"
+//console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
 //console.log(blockVar);
@@ -27,6 +27,28 @@ function show(){
     }
     show();
     
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+//console.log(functionVar); // Throws ReferenceError
+//console.log(functionLet); // Throws ReferenceError
+//console.log(functionConst); // Throws ReferenceError
+
+{
+    var capvar = "capvar";
+    let caplet = "caplet";
+    const capconst = "capconst";
+
+    capvar = "2capvar" // Ok
+    caplet = "2caplet" // Ok
+    //capconst = "2capconst" // da error
+
+    console.log(capvar);
+    console.log(caplet);
+    console.log(capconst);
+}
+
+capvar = "3capvar" // Ok
+caplet = "3caplet" // Ok
+capconst = "3capconst" // Ok
+
+console.log(capvar);
+console.log(caplet);
+console.log(capconst);
